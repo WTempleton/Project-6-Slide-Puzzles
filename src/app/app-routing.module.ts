47364a43c8f87f6c384305752fe460b85c896bc8
WilliamTempleton <william.templeton@centrale-marseille.fr>
2020-03-18 23:53:03 +0100

    Configured all three components: HTML, CSS and TypeScript files. Configured routing links. Installed lodash for array randomization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainMenuComponent } from "./main-menu/main-menu.component";
+import { ThreeByThreeComponent } from "./three-by-three/three-by-three.component";
+import { FourByFourComponent } from "./four-by-four/four-by-four.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "main-menu", component: MainMenuComponent },
+    { path: "three-by-three", component: ThreeByThreeComponent },
+    { path: "four-by-four", component: FourByFourComponent },
+    { path: "**", component: MainMenuComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/four-by-four/four-by-four.component.ts
+++ src/app/four-by-four/four-by-four.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as _ from "lodash";
 
 @Component({
-  selector: 'app-four-by-four',
-  templateUrl: './four-by-four.component.html',
-  styleUrls: ['./four-by-four.component.css']
+    selector: "app-four-by-four",
+    templateUrl: "./four-by-four.component.html",
+    styleUrls: ["./four-by-four.component.css"]
 })
 export class FourByFourComponent implements OnInit {
+    tilesSet: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+    tiles: number[] = _.sampleSize(this.tilesSet, 16);
 
-  constructor() { }
+    zeroTile = "grid-zero";
 
-  ngOnInit(): void {
-  }
+    newGame() {
+        this.tiles = _.sampleSize(this.tilesSet, 16);
+    }
 
+    isZero(value) {
+        if (value === 0) {
+            return false;
+        }
+        return true;
+    }
+
+    canSwap(tile) {
+        const tileIndex = this.tiles.indexOf(tile);
+        const zeroIndex = this.tiles.indexOf(0);
+        if (tileIndex === zeroIndex + 4 || tileIndex === zeroIndex - 4) {
+            return true;
+        }
+        if (
+            tileIndex === zeroIndex + 1 &&
+            tileIndex !== 4 &&
+            tileIndex !== 8 &&
+            tileIndex !== 12
+        ) {
+            return true;
+        }
+        if (
+            tileIndex === zeroIndex - 1 &&
+            tileIndex !== 3 &&
+            tileIndex !== 7 &&
+            tileIndex !== 11
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    swap(tile) {
+        if (this.canSwap(tile) === true) {
+            const tileIndex = this.tiles.indexOf(tile);
+            const zeroIndex = this.tiles.indexOf(0);
+            this.tiles[tileIndex] = 0;
+            this.tiles[zeroIndex] = tile;
+        }
+    }
+
+    arraysEqual(a, b) {
+        for (var i = 0; i < a.length; ++i) {
+            if (a[i] !== b[i]) return false;
+        }
+        return true;
+    }
+
+    gameOn() {
+        if (this.arraysEqual(this.tiles, this.tilesSet)) {
+            return false;
+        }
+        return true;
+    }
+
+    constructor() {}
+
+    ngOnInit(): void {}
 }
